57040e3a298782e9f7f3cbd04807b7c9db5d46ec
Hema Lohana <hemalohana8@gmail.com>
2023-12-07 18:05:37 +0530

    connection string removed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const router = require('./src/routes/route');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://hemalohana8:KwLLh0p6frs8L5m0@cluster0.bff2piv.mongodb.net/").then(()=>{console.log("connected to database")}).catch((err)=>{console.log})
+mongoose.connect(process.env.MONGO_STRING).then(()=>{console.log("connected to database")}).catch((err)=>{console.log(err.message)})
 app.use(express.json());
 app.use('/',router);
 app.listen(process.env.PORT || 3000,(err)=>{
